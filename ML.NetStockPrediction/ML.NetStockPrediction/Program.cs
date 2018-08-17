fa1e0c415d7eb9a14b00b2ae9dbebaafd9bffb88
jufren <tan.jufren@stengg.com>
2018-08-17 14:06:23 +0800

    Add project files.

---
+++ ML.NetStockPrediction/ML.NetStockPrediction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML.NetStockPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
